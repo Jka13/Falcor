80065f7f1b8fd08313a0e70ab74ca4a3d594a3fd
Jonas Kastning <jonaskastning@gmx.de>
2026-06-17 14:30:12 +0200

    fixed reconnection jacobian

--- Source/RenderPasses/ComplexLuminairesReSTIR_PT/ComplexLuminairesReSTIR_PT.h
+++ Source/RenderPasses/ComplexLuminairesReSTIR_PT/ComplexLuminairesReSTIR_PT.h
@@ -151,7 +151,7 @@ private:
     bool mOptionsChanged = false;
 
     //PT
-    uint mMaxBounces = 0;
+    uint mMaxBounces = 6;
     bool mComputeDirect = true;
     bool mUseImportanceSampling = true;
     bool mUseDirectVPLs = true;
